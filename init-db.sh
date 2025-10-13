9404ac21c21a43cb392403e17a94aa2a2e6617c0
M-Alhaig <mohammedalhaij100@gmail.com>
2025-10-13 17:44:37 +0300

    Enable Redis persistence with volume, improve database initialization script, and expand `.gitignore` rules

--- init-db.sh
+++ init-db.sh
@@ -5,10 +5,10 @@ set -e
 # It reads from POSTGRES_MULTIPLE_DATABASES environment variable
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE user_db;
-    CREATE DATABASE item_db;
-    CREATE DATABASE bidding_db;
-    CREATE DATABASE notification_db;
+    CREATE DATABASE IF NOT EXISTS user_db;
+    CREATE DATABASE IF NOT EXISTS item_db;
+    CREATE DATABASE IF NOT EXISTS bidding_db;
+    CREATE DATABASE IF NOT EXISTS notification_db;
 EOSQL
 
 echo "Multiple databases created successfully!"
