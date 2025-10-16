da55d2f9e83a3a5e92f57f278a34d0e23770f62d
M-Alhaig <mohammedalhaij100@gmail.com>
2025-10-16 21:58:22 +0300

    feat: add AuctionEndedEvent and listener for auction finalization
    - Introduced `AuctionEndedEvent` to encapsulate auction end details with metadata.
    - Implemented `AuctionEventListener` to handle auction finalization by marking auctions as ended in the cache.

---
+++ bidding-service/src/main/java/com/auction/biddingservice/listeners/AuctionEventListener.java
@@ -0,0 +1,23 @@
+package com.auction.biddingservice.listeners;
+
+import com.auction.biddingservice.events.AuctionEndedEvent;
+import com.auction.biddingservice.services.AuctionCacheService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuctionEventListener {
+
+	private final RabbitTemplate rabbitTemplate;
+	private final AuctionCacheService auctionCacheService;
+
+	private static final String QUEUE_NAME = "AuctionEndedQueue";
+
+	@RabbitListener(queues = "#{auctionEndedQueue.name}")
+	public void onAuctionEnded(AuctionEndedEvent event) {
+		auctionCacheService.markAuctionEnded(event.data().itemId(), event.data().endTime());
+	}
+}
