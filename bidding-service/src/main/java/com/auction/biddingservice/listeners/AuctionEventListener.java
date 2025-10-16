62f70f12c641712cc78e3719dea371aa403e2e7f
M-Alhaig <mohammedalhaij100@gmail.com>
2025-10-16 22:04:59 +0300

    feat: add AuctionCacheService for auction end state management
    - Implemented `AuctionCacheService` to manage auction end state using Redis.
    - Updated `AuctionEventListener` to utilize `AuctionCacheService` and remove unused `RabbitTemplate`.

--- bidding-service/src/main/java/com/auction/biddingservice/listeners/AuctionEventListener.java
+++ bidding-service/src/main/java/com/auction/biddingservice/listeners/AuctionEventListener.java
@@ -4,14 +4,12 @@ import com.auction.biddingservice.events.AuctionEndedEvent;
 import com.auction.biddingservice.services.AuctionCacheService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class AuctionEventListener {
 
-	private final RabbitTemplate rabbitTemplate;
 	private final AuctionCacheService auctionCacheService;
 
 	private static final String QUEUE_NAME = "AuctionEndedQueue";
---
+++ bidding-service/src/main/java/com/auction/biddingservice/services/AuctionCacheService.java
@@ -0,0 +1,33 @@
+package com.auction.biddingservice.services;
+
+import java.time.Instant;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AuctionCacheService {
+
+	public static final String AUCTION_ENDTIME_KEY_PREFIX = "auction:endtime:";
+	private final RedisTemplate<String, String> redisTemplate;
+
+	public void markAuctionEnded(Long itemId, Instant endTime) {
+
+		redisTemplate.opsForValue().set(AUCTION_ENDTIME_KEY_PREFIX + itemId, endTime.toString());
+
+	}
+	public boolean isAuctionEnded(Long itemId) {
+		boolean isEnded = redisTemplate.hasKey(AUCTION_ENDTIME_KEY_PREFIX + itemId);
+
+		if (isEnded) {
+			log.debug("Auction {} ended at {}", itemId, redisTemplate.opsForValue().get(AUCTION_ENDTIME_KEY_PREFIX + itemId));
+		} else {
+			log.debug("Auction {} is still active", itemId);
+		}
+
+		return isEnded;
+	}
+}
